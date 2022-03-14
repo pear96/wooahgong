57ef7341e9d84eea44721ccfc617954a4d200939
jeehwan <meanstrike94@gmail.com>
2022-03-14 14:24:29 +0900

    frontend 초기세팅

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function App() {
+  return <>초기세팅 성공</>;
+}
+export default App;
---
+++ frontend/src/app/rootReducer.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from '@reduxjs/toolkit';
+
+// 만들어 놓은 리듀서들을 합친다.
+const reducer = combineReducers({});
+
+// React에서 사용할 수 있도록 타입을 만들어 export 해준다.
+export type ReducerType = ReturnType<typeof reducer>;
+export default reducer;

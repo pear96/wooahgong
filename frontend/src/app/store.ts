f5b4f6ba6d1e7ff726c53ee503bbb6aff5359f93
jeehwan <meanstrike94@gmail.com>
2022-03-14 15:28:59 +0900

    프로젝트 세팅 수정app

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
 import reducer from './rootReducer';
 
 const store = configureStore({
   reducer,
 });
 
-export type AppDispatch = typeof store.dispatch;
 export default store;
+
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();

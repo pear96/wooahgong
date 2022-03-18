f3811ec044763f42b396109c925a5e37ded95cc8
jeehwan <meanstrike94@gmail.com>
2022-03-18 17:14:38 +0900

    S06P22A505-116 [Add] 일반 로그인 페이지 구현 및 authSlice 비동기 통신 구현

--- frontend/src/features/Auth/authSlice.ts
+++ frontend/src/features/Auth/authSlice.ts
@@ -1,12 +1,14 @@
 /* eslint-disable no-param-reassign */
 import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { saveToken } from '../../common/api/JTW-Token';
 
-// 서버로 code 전송
-export const postKakacode = createAsyncThunk('auth/postKakacode', async (data: string, { rejectWithValue }) => {
+// 서버로 code 전송 => socical login
+export const postKakaocode = createAsyncThunk('auth/postKakaocode', async (data: string, { rejectWithValue }) => {
   try {
     console.dir(data);
     const response = await axios.post('백코드');
+    // 여기서 소셜 로그인의 분기가 이루어진다
     return response;
   } catch (error: any) {
     console.dir(error);
@@ -14,11 +16,16 @@ export const postKakacode = createAsyncThunk('auth/postKakacode', async (data: s
   }
 });
 
-// 서버로 부터 토큰 받아옴
-export const getToken = createAsyncThunk('auth/getToken', async (data, { rejectWithValue }) => {
+// 일반 login
+export interface Data {
+  id: string;
+  password: string;
+}
+export const commonLogin = createAsyncThunk('auth/getToken', async (data: Data, { rejectWithValue }) => {
   try {
     console.dir(data);
     const response = await axios.get('백코드');
+    saveToken(response.data.accessToken); // 토큰 저장
     return response;
   } catch (error: any) {
     console.dir(error);
@@ -27,50 +34,66 @@ export const getToken = createAsyncThunk('auth/getToken', async (data, { rejectW
 });
 
 export interface InitialState {
-  code: string;
+  nickname: string;
+  profileImg: string;
   postkakaoCodeLoading: boolean;
-  postkakaoCodeDong: boolean;
+  postkakaoCodeDone: boolean;
   postkakaoCodeError: string;
-  getTokenLoading: boolean;
-  getTokenDong: boolean;
-  getTokenError: string;
+  commonLoginLoading: boolean;
+  commonLoginDone: boolean;
+  commonLoginError: string;
 }
 
 const initialState: InitialState = {
-  code: '',
+  nickname: '',
+  profileImg: '',
   postkakaoCodeLoading: false,
-  postkakaoCodeDong: false,
+  postkakaoCodeDone: false,
   postkakaoCodeError: '',
-  getTokenLoading: false,
-  getTokenDong: false,
-  getTokenError: '',
+  commonLoginLoading: false,
+  commonLoginDone: false,
+  commonLoginError: '',
 };
 const authSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    saveCode: (state, action: PayloadAction<string>) => {
-      state.code = action.payload;
+    setUser: (state, action: PayloadAction<{ nickname: string; profileImg: string }>) => {
+      state.nickname = action.payload.nickname;
+      state.profileImg = action.payload.profileImg;
     },
   },
   extraReducers: (builder) => {
     builder
-      .addCase(postKakacode.pending, (state) => {
+      .addCase(postKakaocode.pending, (state) => {
         state.postkakaoCodeLoading = true;
-        state.postkakaoCodeDong = false;
+        state.postkakaoCodeDone = false;
         state.postkakaoCodeError = '';
       })
-      .addCase(postKakacode.fulfilled, (state) => {
+      .addCase(postKakaocode.fulfilled, (state) => {
         state.postkakaoCodeLoading = false;
-        state.postkakaoCodeDong = true;
+        state.postkakaoCodeDone = true;
       })
-      .addCase(postKakacode.rejected, (state, action) => {
+      .addCase(postKakaocode.rejected, (state, action) => {
         state.postkakaoCodeLoading = true;
         state.postkakaoCodeError = action.error.message as string;
+      })
+      .addCase(commonLogin.pending, (state) => {
+        state.commonLoginLoading = true;
+        state.commonLoginDone = false;
+        state.commonLoginError = '';
+      })
+      .addCase(commonLogin.fulfilled, (state) => {
+        state.commonLoginLoading = false;
+        state.commonLoginDone = true;
+      })
+      .addCase(commonLogin.rejected, (state, action) => {
+        state.commonLoginLoading = true;
+        state.commonLoginError = action.error.message as string;
       });
   },
 });
 
-export const { saveCode } = authSlice.actions;
+export const { setUser } = authSlice.actions;
 
 export default authSlice.reducer;

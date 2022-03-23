d58b6b0c0f6fd63ab5945f0a39d717250dce9f19
엄희성 <ehs0525@naver.com>
2022-03-23 14:44:31 +0900

    #S06P22A505-133 [Add] 프로필 수정 페이지 일부 추가

--- frontend/src/features/Profile/profileUpdate.tsx
+++ frontend/src/features/Profile/profileUpdate.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Navbar from 'common/Navbar';
+import ProfileUpdateHeader from './components/update/ProfileUpdateHeader';
+import ProfileImageUpdate from './components/update/ProfileImageUpdate';
 
 function ProfileUpdate() {
   return (
     <>
       <Navbar />
-      <div>프로필 수정 페이지</div>
+      <div style={{ marginLeft: 'auto', marginRight: 'auto', maxWidth: '1024px' }}>
+        <ProfileUpdateHeader />
+        <ProfileImageUpdate />
+        <div>
+          <div>사진</div>
+          <div>프로필 사진 변경</div>
+        </div>
+        <div>수정 부분</div>
+        <div>우아공 떠나기</div>
+      </div>
     </>
   );
 }

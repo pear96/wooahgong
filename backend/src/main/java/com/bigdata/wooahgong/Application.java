9c8a9200cb873a761b7773d7cedb75264f2921b1
qweadzs <qweadzs@naver.com>
2022-03-25 19:02:12 +0900

    #S06P22A505-193 [fix] ssl 다시 롤백

--- backend/src/main/java/com/bigdata/wooahgong/Application.java
+++ backend/src/main/java/com/bigdata/wooahgong/Application.java
@@ -12,7 +12,6 @@ public class Application {
             + "classpath:application-db.properties,"
             + "classpath:application-social.properties,"
             + "classpath:application-aws.properties,"
-            + "classpath:application-key.properties,"
             + "classpath:application-email.properties";
 
     public static void main(String[] args) {

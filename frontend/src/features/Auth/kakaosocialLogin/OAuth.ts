7e17f9e7e4ea80039cdcf56cde42a14451cf2300
Hong Seung-Gi <tmdrl5661@naver.com>
2022-03-25 16:26:59 +0900

    [Add] 카카오 env 설정

--- frontend/src/features/Auth/kakaosocialLogin/OAuth.ts
+++ frontend/src/features/Auth/kakaosocialLogin/OAuth.ts
@@ -1,4 +1,4 @@
-const CLIENT_ID = 'c5a2bd6ef44da44acf32da1e7be194e0';
-const REDIRECT_URI = 'http://localhost:3000/oauth/callback/kakao';
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;

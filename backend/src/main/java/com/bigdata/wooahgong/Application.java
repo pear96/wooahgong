a946dc3bd95c0a53468987eae086a200e41596c7
amlwq <bhaeun1105@gmail.com>
2022-03-26 16:51:29 +0900

    [Fix] Application.Java에 , 하나 안써서 빌드 안됨~

--- backend/src/main/java/com/bigdata/wooahgong/Application.java
+++ backend/src/main/java/com/bigdata/wooahgong/Application.java
@@ -12,7 +12,7 @@ public class Application {
             + "classpath:application-db.properties,"
             + "classpath:application-social.properties,"
             + "classpath:application-aws.properties,"
-            + "classpath:application-email.properties"
+            + "classpath:application-email.properties,"
             + "classpath:application-ssl.properties";
 
     public static void main(String[] args) {

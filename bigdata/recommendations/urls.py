b02be611d03acb0dd078c9f8b53feddccd0c5176
amlwq <bhaeun1105@gmail.com>
2022-03-29 01:32:23 +0900

    #S06P22A505-216 [Add] Fast API로 변환

--- bigdata/recommendations/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'recommendations'
-urlpatterns = [
-    path('forme/', views.forme, name='forme'),
-    path('trend/', views.trend, name="trend")
-]

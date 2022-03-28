8dcb2ab271014d2e86973b100c8e794f2bfdfdfb
Hong Seung-Gi <tmdrl5661@naver.com>
2022-03-28 12:48:01 +0900

    #S06P22A505-207 [Add] 피드 이미지 업로드 구현

--- frontend/src/features/Feed/Feedadd.tsx
+++ frontend/src/features/Feed/Feedadd.tsx
@@ -3,15 +3,16 @@ import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import FeedImage from './FeedImage';
+import SearchPlace from './SearchPlace';
 import { feed, setImage, setType } from './feedReducer';
 import { ReducerType } from '../../app/rootReducer';
 
 const Container = styled.div`
-  position: relative;
-  width: 360px;
-  height: 720px;
-  background: none;
-  margin: 0 auto;
+    position: relative;
+    width: 360px;
+    height: 720px;
+    background: none;
+    margin: 0 auto;
 `;
 
 function Feedsadd() {
@@ -22,6 +23,7 @@ function Feedsadd() {
         <Container>
             <Routes>
                 <Route path="/" element={<FeedImage/>} />
+                <Route path="/searchplace" element={<SearchPlace/>}/>
             </Routes>
         </Container>
     );
--- frontend/src/features/Feed/feedReducer.ts
+++ frontend/src/features/Feed/feedReducer.ts
@@ -3,20 +3,25 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 export interface Feed{
     image : File[],
     type : boolean,
+    place : {placeSeq : number, name : string, address : string }
 }
 
 export const feed = createSlice({
     name : 'feed',
     initialState: {
         image : [],
-        type : false
+        type : false,
+        place : {placeSeq : 0, name : "", address : "" }
     } as Feed,
     reducers: {
-        setImage(state, action : PayloadAction<File>){
-            state.image.push(action.payload);
+        setImage(state, action : PayloadAction<File[]>){
+            state.image = action.payload;
         },
         setType(state, action : PayloadAction<boolean>){
             state.type = action.payload;
+        },
+        setPlace(state, action : PayloadAction<{placeSeq : number, name : string, address : string }>){
+            state.place = action.payload;
         },
         setInitState(state){
             state.image = [];
@@ -24,5 +29,5 @@ export const feed = createSlice({
         }
     }
 })
-export const {setImage, setType, setInitState} = feed.actions;
+export const {setImage, setType, setPlace,setInitState} = feed.actions;
 export default feed.reducer;

327ca2d4cf3091a0734d064b47db617afc3d41ff
qweadzs <qweadzs@naver.com>
2022-03-29 10:38:05 +0900

    #S06P22A505-212 [fix] 장소 상세 response 위도 경도 추가

--- backend/src/main/java/com/bigdata/wooahgong/place/dtos/response/DetailPlaceRes.java
+++ backend/src/main/java/com/bigdata/wooahgong/place/dtos/response/DetailPlaceRes.java
@@ -12,16 +12,19 @@ public class DetailPlaceRes {
     private final String name;
     private final String address;
     private final Double avgRatings;
+    private final Double latitude;
+    private final Double longitude;
     private final String placeImageUrl;
     private final List<CustomFeedDto> feeds;
     private final Boolean isWished;
 
     @Builder
-    public DetailPlaceRes(String name, String address, Double avgRatings,
-                          String placeImageUrl, List<CustomFeedDto> feeds, Boolean isWished) {
+    public DetailPlaceRes(String name, String address, Double avgRatings, Double latitude, Double longitude, String placeImageUrl, List<CustomFeedDto> feeds, Boolean isWished) {
         this.name = name;
         this.address = address;
         this.avgRatings = avgRatings;
+        this.latitude = latitude;
+        this.longitude = longitude;
         this.placeImageUrl = placeImageUrl;
         this.feeds = feeds;
         this.isWished = isWished;

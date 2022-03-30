ea84ffcd0eecae3f319a0063b8b0eebfc9404776
amlwq <bhaeun1105@gmail.com>
2022-03-30 14:07:08 +0900

    #S06P22A505-216 [Update] token 까서 email 찾아서 사용자 찾기 성공

--- bigdata/app/routes/recommend.py
+++ bigdata/app/routes/recommend.py
@@ -22,23 +22,21 @@ async def forme(request: Request, searchRadius: int, lat: float, lng: float, ses
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    print("----------------request 출력---------------")
     token = request.headers.get('Authorization')
+    # 그 Authorization으로 받은 내용을 JWT로 해석하여서 email을 추출해냄
     try:
         token = token.replace(PREFIX, "")
-        user_email = jwt.decode(token, SECRET_KEY, [ALGORITHM])["sub"] # {'sub': 'amlwq@naver.com', 'iss': 'ssafy.com', 'exp': 1649907298, 'iat': 1648611298}
-        print(user_email) 
+        user_email = jwt.decode(token, SECRET_KEY, [ALGORITHM])["sub"] # {'sub': 'amlwq@naver.com', 'iss': 'ssafy.com', 'exp': 1649907298, 'iat': 1648611298} 
     except JWTError:
         raise credentials_exception
-    print("----------------request 출력---------------")
-    # 그 Authorization으로 받은 내용을 JWT로 해석하여서 email을 추출해냄
     # 해당 email로 user를 찾음
     print("----------------찾은 유저 출력---------------")
-    user = session.query(User).filter(User.email == user_email)
+    print(user_email)
+    user = session.query(User).filter(User.email == user_email).all()
     print(user)
     print("----------------찾은 유저 출력---------------")
-    data = {"user": user}
-    return data
+    # data = {"user": user}
+    return user
 
 
 @router.get("/trend")

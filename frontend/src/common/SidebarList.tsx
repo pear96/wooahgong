b42b565ef0e58ac1590f409035cb931f181c0395
Hong Seung-Gi <tmdrl5661@naver.com>
2022-04-02 11:44:42 +0900

    [Add] 지도 범위 조절 기능 추가, 네비바 스타일 변경

--- frontend/src/common/SidebarList.tsx
+++ frontend/src/common/SidebarList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import HomeIcon from 'assets/menu/home.png';
-import ProfileIcon from 'assets/menu/profile.png';
+import MapIcon from 'assets/menu/mapicon.png';
 import ReportIcon from 'assets/menu/report.png';
 import LogoutIcon from 'assets/menu/logout.png';
 
@@ -13,22 +13,22 @@ const styles = {
 
 export const SidebarList = [
   {
-    title: '홈',
+    title: 'HOME',
     path: '/main',
     icon: <img style={styles} src={HomeIcon} alt="HomeIcon" />,
   },
   {
-    title: '프로필',
-    path: `/profile`,
-    icon: <img style={styles} src={ProfileIcon} alt="ProfileIcon" />,
+    title: 'MAP',
+    path: `/map`,
+    icon: <img style={styles} src={MapIcon} alt="ProfileIcon" />,
   },
   {
-    title: '제보하기',
+    title: 'REPORT',
     path: '/report',
     icon: <img style={styles} src={ReportIcon} alt="ReportIcon" />,
   },
   {
-    title: '로그아웃',
+    title: 'LOGOUT',
     path: '/',
     icon: <img style={styles} src={LogoutIcon} alt="LogoutIcon" />,
   },

e7349c77e6fa4d90c946a8d069648c2bca1f7167
qweadzs <qweadzs@naver.com>
2022-04-05 15:02:48 +0900

    fix

--- frontend/src/common/api/ProfileApi.ts
+++ frontend/src/common/api/ProfileApi.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { getToken } from './JTW-Token';
 
-// const BASE_URL = 'https://j6a505.p.ssafy.io/api/users';
-const BASE_URL = 'http://localhost:8080/api/users';
+const BASE_URL = 'https://j6a505.p.ssafy.io/api/users';
+// const BASE_URL = 'http://localhost:8080/api/users';
 const token = getToken();
 
 const getProfile = async (nickname: string) => {

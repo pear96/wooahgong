fff1d65a685c5200e39775dffdacbd9236b4c69f
qweadzs <qweadzs@naver.com>
2022-04-05 16:54:17 +0900

    fix

--- frontend/src/common/api/ProfileApi.ts
+++ frontend/src/common/api/ProfileApi.ts
@@ -3,7 +3,6 @@ import { getToken } from './JTW-Token';
 
 const BASE_URL = 'https://j6a505.p.ssafy.io/api/users';
 // const BASE_URL = 'http://localhost:8080/api/users';
-const token = getToken();
 
 const getProfile = async (nickname: string) => {
   const token = getToken();
@@ -52,129 +51,129 @@ const getProfileForUpdate = async (nickname: string) => {
   return result;
 };
 
-const getMyFeeds = async (data : {nickname : string, page : number}) => {
+const getMyFeeds = async (data: { nickname: string, page: number }) => {
   const token = getToken();
   console.log(data);
   const result = await axios.get(`${BASE_URL}/${data.nickname}/feeds?page=${data.page}`,
-                                  {headers : { Authorization: `${token}` }})
-                            .then((response)=>{
-                              const value = {
-                                status : response.status,
-                                data : response.data
-                              }
-                              return value;
-                            }).catch((e)=>{
-                              const value = {
-                                status : 401,
-                                data : null
-                              }
-                              return value;
-                            })
+    { headers: { Authorization: `${token}` } })
+    .then((response) => {
+      const value = {
+        status: response.status,
+        data: response.data
+      }
+      return value;
+    }).catch((e) => {
+      const value = {
+        status: 401,
+        data: null
+      }
+      return value;
+    })
   console.log(result);
   return result;
 };
 
-const getLikedFeeds = async (data : {nickname : string, page : number}) => {
+const getLikedFeeds = async (data: { nickname: string, page: number }) => {
   const token = getToken();
   const result = await axios.get(`${BASE_URL}/${data.nickname}/liked?page=${data.page}`,
-                            {headers : { Authorization: `${token}` }})
-                          .then((response)=>{
-                          const value = {
-                          status : response.status,
-                          data : response.data
-                          }
-                          return value;
-                          }).catch((e)=>{
-                          const value = {
-                          status : 401,
-                          data : null
-                          }
-                          return value;
-                          });
+    { headers: { Authorization: `${token}` } })
+    .then((response) => {
+      const value = {
+        status: response.status,
+        data: response.data
+      }
+      return value;
+    }).catch((e) => {
+      const value = {
+        status: 401,
+        data: null
+      }
+      return value;
+    });
   console.log(result);
   return result;
 };
 
-const getWishedFeeds = async (data : {nickname : string, page : number}) => {
+const getWishedFeeds = async (data: { nickname: string, page: number }) => {
   console.log(data);
   const token = getToken();
   const result = await axios.get(`${BASE_URL}/${data.nickname}/wished?page=${data.page}`,
-                                  {headers : { Authorization: `${token}` }})
-                            .then((response)=>{
-                              const value = {
-                                status : response.status,
-                                data : response.data
-                              }
-                              return value;
-                            }).catch((e)=>{
-                              const value = {
-                                status : 401,
-                                data : null
-                              }
-                              return value;
-                            })
+    { headers: { Authorization: `${token}` } })
+    .then((response) => {
+      const value = {
+        status: response.status,
+        data: response.data
+      }
+      return value;
+    }).catch((e) => {
+      const value = {
+        status: 401,
+        data: null
+      }
+      return value;
+    })
   console.log(result);
   return result;
 };
 
 const updateProfile = async (nickname: string | null, data: { nickname: string; mbti: string; moods: string[] }) => {
   const token = getToken();
-  if(nickname === null) return {status : 400, data : null};
-  const result = await axios.patch(`${BASE_URL}/${nickname}`, data,{headers: { Authorization: `${token}`}})
-                          .then((response)=>{
-                            const value = {
-                              status : response.status
-                            }
-                            return value;
-                          })
-                          .catch((e)=>{
-                            const value = {
-                              status : 403
-                            }
-                            return value;
-                          })
-  return result;                    
+  if (nickname === null) return { status: 400, data: null };
+  const result = await axios.patch(`${BASE_URL}/${nickname}`, data, { headers: { Authorization: `${token}` } })
+    .then((response) => {
+      const value = {
+        status: response.status
+      }
+      return value;
+    })
+    .catch((e) => {
+      const value = {
+        status: 403
+      }
+      return value;
+    })
+  return result;
 };
 
 const updateProfileImage = async (nickname: string | null, data: FormData) => {
   const token = getToken();
-  if(token && nickname !== null){
-    const result = await axios.patch(`${BASE_URL}/${nickname}/profileimg`, data, {headers : {Authorization: token, 'Content-type' : "multipart/form-data"}})
-                            .then((response)=>{
-                                const value = {
-                                  status : response.status,
-                                  data : response.data
-                                }
-                                return value;
-                            })
-                            .catch((e)=>{
-                              const value = {
-                                status : 403,
-                                data : null
-                              }
-                              return value;
-                            })
+  if (token && nickname !== null) {
+    const result = await axios.patch(`${BASE_URL}/${nickname}/profileimg`, data, { headers: { Authorization: token, 'Content-type': "multipart/form-data" } })
+      .then((response) => {
+        const value = {
+          status: response.status,
+          data: response.data
+        }
+        return value;
+      })
+      .catch((e) => {
+        const value = {
+          status: 403,
+          data: null
+        }
+        return value;
+      })
     return result;
   }
-  return {status : 400, data : null};
+  return { status: 400, data: null };
 };
-const getPwdChangeResult = async (body : {userId : string, password : string}) => {
+const getPwdChangeResult = async (body: { userId: string, password: string }) => {
   const token = getToken();
   const result = await axios.patch(`https://j6a505.p.ssafy.io/api/users/repwd`, body)
-                          .then((response)=>{
-                            const value = {
-                              status : response.status
-                            }
-                            return value;
-                          })
-                          .catch((e)=>{
-                            const value = {
-                              status : 400
-                            }
-                            return value;
-                          })
+    .then((response) => {
+      const value = {
+        status: response.status
+      }
+      return value;
+    })
+    .catch((e) => {
+      const value = {
+        status: 400
+      }
+      return value;
+    })
   return result;
-}         
+}
 
 
 const resign = (nickname: string) => {

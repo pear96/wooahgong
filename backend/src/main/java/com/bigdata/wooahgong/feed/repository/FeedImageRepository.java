a08c76d32ddf20eaabb868a6998c2f78d58516cb
pear96 <bhaeun1105@gmail.com>
2023-08-10 19:30:21 +0900

    refact : 피드의 이미지 찾는 방식 수정
    - JPQL을 활용해 피드 seq 로 이미지 Path를 반환하도록 수정

--- backend/src/main/java/com/bigdata/wooahgong/feed/repository/FeedImageRepository.java
+++ backend/src/main/java/com/bigdata/wooahgong/feed/repository/FeedImageRepository.java
@@ -3,10 +3,12 @@ package com.bigdata.wooahgong.feed.repository;
 import com.bigdata.wooahgong.feed.entity.Feed;
 import com.bigdata.wooahgong.feed.entity.FeedImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface FeedImageRepository extends JpaRepository<FeedImage, Long> {
-    Optional<List<FeedImage>> findAllByFeed(Feed feed);
+    @Query("select i.imageUrl from FeedImage i where i.feed.feedSeq = :feedSeq")
+    Optional<List<String>> findImageUrlAllByFeed(Long feedSeq);
 }

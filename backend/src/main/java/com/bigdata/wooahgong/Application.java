32f50f35eb2947b2697057d4ba9ca341cd458f40
pear96 <bhaeun1105@gmail.com>
2023-08-10 19:27:21 +0900

    refact : Application 의 classpath 제거 및 yml에 추가

--- backend/src/main/java/com/bigdata/wooahgong/Application.java
+++ backend/src/main/java/com/bigdata/wooahgong/Application.java
@@ -7,16 +7,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class Application {
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.properties,"
-            + "classpath:application-db.properties,"
-            + "classpath:application-social.properties,"
-            + "classpath:application-aws.properties,"
-            + "classpath:application-email.properties";
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class)
-                .properties(APPLICATION_LOCATIONS)
                 .run(args);
     }
 }

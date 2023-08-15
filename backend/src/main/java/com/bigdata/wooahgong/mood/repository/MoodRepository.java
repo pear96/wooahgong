82f8b6c949943aaa5c2cc51ad3d394fc9fae7f01
pear96 <bhaeun1105@gmail.com>
2023-08-15 16:24:04 +0900

    feat : MoodRepository에 MoodSeq로 찾는 메서드 추가

--- backend/src/main/java/com/bigdata/wooahgong/mood/repository/MoodRepository.java
+++ backend/src/main/java/com/bigdata/wooahgong/mood/repository/MoodRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface MoodRepository extends JpaRepository<Mood, Long> {
     Optional<Mood> findByMoodContaining(String mood);
+    Mood findByMoodSeq(Long moodSeq);
 }

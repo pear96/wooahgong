559260ff4e4af94a8279fdcb030b0e33fc6b79e8
pear96 <bhaeun1105@gmail.com>
2023-08-18 03:39:06 +0900

    refact : 에러 헨들러 수정
    - CustomException이 아니면 다 중복 데이터 처리가 되었음

--- backend/src/main/java/com/bigdata/wooahgong/common/exception/GlobalResponseExceptionHandler.java
+++ backend/src/main/java/com/bigdata/wooahgong/common/exception/GlobalResponseExceptionHandler.java
@@ -1,24 +1,15 @@
 package com.bigdata.wooahgong.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.exception.ConstraintViolationException;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import static com.bigdata.wooahgong.common.exception.ErrorCode.*;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
-    @ExceptionHandler(value = { ConstraintViolationException.class, DataIntegrityViolationException.class})
-    protected ResponseEntity<ErrorResponse> handleDataException() {
-        log.error("handleDataException throw Exception : {}", DUPLICATE_RESOURCE);
-        return ErrorResponse.toResponseEntity(DUPLICATE_RESOURCE);
-    }
+public class GlobalResponseExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = { CustomException.class })
     protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {

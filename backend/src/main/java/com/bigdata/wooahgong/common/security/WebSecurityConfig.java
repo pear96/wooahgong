2ff19b1368442879e96fc27d2aecd45c6bcf9fc6
haeun <bhaeun1105@gmail.com>
2025-04-09 02:00:57 +0900

    s

--- backend/src/main/java/com/bigdata/wooahgong/common/security/WebSecurityConfig.java
+++ backend/src/main/java/com/bigdata/wooahgong/common/security/WebSecurityConfig.java
@@ -1,71 +1,25 @@
-// package com.bigdata.wooahgong.common.security;
-//
-// import com.bigdata.wooahgong.common.util.JwtTokenUtil;
-// import org.springframework.boot.web.servlet.FilterRegistrationBean;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.annotation.web.builders.WebSecurity;
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-// import org.springframework.security.config.http.SessionCreationPolicy;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.web.cors.CorsConfiguration;
-// import org.springframework.web.cors.CorsConfigurationSource;
-// import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-// import org.springframework.web.filter.CommonsRequestLoggingFilter;
-//
-// import javax.servlet.Filter;
-//
-// @Configuration
-// @EnableWebSecurity
-// public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//     @Bean
-//     public PasswordEncoder getPasswordEncoder() {
-//         return new BCryptPasswordEncoder();
-//     }
-//
-//     @Override
-//     protected void configure(HttpSecurity http) throws Exception {
-//         http
-//                 .httpBasic().disable()
-//                 .csrf().disable()
-//                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 사용 하지않음
-//                 .and()
-//                 .authorizeRequests()
-//                 .anyRequest().permitAll()
-//                 .and().cors();
-//     }
-//     @Override public void configure(WebSecurity web) throws Exception { web.ignoring().antMatchers( "/static/css/**", "/static/js/**", "/static/img/**"); }
-//     @Bean
-//     public CorsConfigurationSource corsConfigurationSource() {
-//         CorsConfiguration configuration = new CorsConfiguration();
-//         // configuration.addAllowedOrigin("*");
-//         configuration.addAllowedOriginPattern("*");
-//         configuration.addAllowedMethod("*");
-//         configuration.addAllowedHeader("*");
-//         configuration.addExposedHeader(JwtTokenUtil.HEADER_STRING);
-//         configuration.setAllowCredentials(true);
-//         configuration.setMaxAge(3600L);
-//         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//         source.registerCorsConfiguration("/**", configuration);
-//         return source;
-//     }
-//     public Filter requestLoggingFilter() {
-//         CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
-//         loggingFilter.setIncludeClientInfo(true);
-//         loggingFilter.setIncludeQueryString(true);
-//         loggingFilter.setIncludePayload(true);
-//         loggingFilter.setIncludeHeaders(true);
-//         loggingFilter.setMaxPayloadLength(64000);
-//         return loggingFilter;
-//     }
-//
-//     @Bean
-//     public FilterRegistrationBean loggingFilterRegistration() {
-//         FilterRegistrationBean registration = new FilterRegistrationBean(requestLoggingFilter());
-//         registration.addUrlPatterns("/api/*");
-//         return registration;
-//     }
-// }
+package com.bigdata.wooahgong.common.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	// WebSecurity 쪽에서 모든 요청을 Security 필터 체인 적용 대상에서 제외
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/**");
+	}
+
+	// HttpSecurity 설정은 기본적으로 허용하지만, 사실상 위에서 모든 URL을 무시하므로 의미가 없습니다.
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().permitAll()
+			.and().csrf().disable();
+	}
+}
